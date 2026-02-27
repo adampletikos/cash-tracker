6b10985a4d4796c95ecb6df7f641be06311f5edf
Sulfa <adampletikos@gmail.com>
2026-02-27 23:12:07 +0100

    findAll() and findByCategoryAndMonth() implemented in CsvBudgetrepository

--- src/main/java/se/adam/cashtracker/repository/BudgetRepository.java
+++ src/main/java/se/adam/cashtracker/repository/BudgetRepository.java
@@ -4,13 +4,15 @@ import se.adam.cashtracker.model.Category;
 import se.adam.cashtracker.model.MonthlyBudget;
 
 import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
 import java.util.List;
 import java.util.Optional;
 
 public interface BudgetRepository {
     void save(MonthlyBudget budget);
 
-    Optional<MonthlyBudget> findByCategoryAndMonth(Category category, Month month);
+    Optional<MonthlyBudget> findByCategoryAndMonth(Category category, YearMonth month);
 
     List<MonthlyBudget> findAll();
 }
--- src/main/java/se/adam/cashtracker/repository/CsvBudgetRepository.java
+++ src/main/java/se/adam/cashtracker/repository/CsvBudgetRepository.java
@@ -1,13 +1,20 @@
 package se.adam.cashtracker.repository;
 
+import se.adam.cashtracker.model.Category;
 import se.adam.cashtracker.model.MonthlyBudget;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class CsvBudgetRepository implements BudgetRepository{
     private final Path filePath;
@@ -16,6 +23,7 @@ public class CsvBudgetRepository implements BudgetRepository{
         this.filePath = filePath;
     }
 
+    @Override
     public void save(MonthlyBudget budget) {
         try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
             writer.write("month,budget,category");
@@ -29,8 +37,44 @@ public class CsvBudgetRepository implements BudgetRepository{
         }
     }
 
+    @Override
     public List<MonthlyBudget> findAll() {
+        List<MonthlyBudget> budgets = new ArrayList<>();
 
+        if (!Files.exists(filePath)) {return budgets;}
+
+        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+            reader.readLine();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] budgetCSV = line.split(",");
+                if (budgetCSV.length != 3) {
+                    throw new IllegalStateException("Invalid budget row: " + line);
+                }
+
+                MonthlyBudget budget = new MonthlyBudget(
+                        YearMonth.parse(budgetCSV[0]),
+                        new BigDecimal(budgetCSV[1]),
+                        Category.valueOf(budgetCSV[2]));
+                budgets.add(budget);
+            }
+
+            return budgets;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read budgets",e);
+        }
     }
 
+    @Override
+    public Optional<MonthlyBudget> findByCategoryAndMonth(Category category, YearMonth month) {
+        List<MonthlyBudget> budgets = findAll();
+        for (MonthlyBudget budget : budgets) {
+            if (budget.getCategory().equals(category) && budget.getMonth().equals(month)) {
+                return Optional.of(budget);
+            }
+        }
+        return Optional.empty();
+    }
 }
